d704c7b63beced11f9e7bf36c0ecf3c587f88319
HilmiZaidanRafi <muhammadhilmizaidanrafi@students.amikom.ac.id>
2026-09-05 14:18:45 +0700

    Fix database connection and turso client

--- db.js
+++ db.js
@@ -1,8 +1,14 @@
-const { createClient } = require('@libsql/client');
+const { createClient } = require('@libsql/client/http'); // Gunakan adapter HTTP khusus
+
+const url = (process.env.TURSO_DATABASE_URL || '').trim();
+const authToken = (process.env.TURSO_AUTH_TOKEN || '').trim();
+
+// Pakai protokol https:// agar cocok dengan adapter /http
+const formattedUrl = url.replace('libsql://', 'https://');
 
 const db = createClient({
-  url: process.env.TURSO_DATABASE_URL || '',
-  authToken: process.env.TURSO_AUTH_TOKEN || ''
+    url: formattedUrl,
+    authToken: authToken
 });
 
 module.exports = db;
